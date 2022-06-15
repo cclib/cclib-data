9bcfb751a165e3f4f0d850bb48656130bc75d8e6
oliver-s-lee <osl@st-andrews.ac.uk>
2022-06-15 09:11:37 +0100

    Added regression test for Turbomole MP2 energies having twice the number of energies

---
+++ Turbomole/Turbomole7.5/mp2_opt/restart.cc
@@ -0,0 +1,23 @@
+$chkbas=     22803.1746520928
+$nucrep=     454.322999564304
+$chkaux=     5718725.02137888
+$chkupro=     1.00000000000000
+$chkipro=     2.00000000000000
+$chklasrep=     1.00000000000000
+$chkisy6=     4.00000000000000
+$chkmos=     114.219321023948
+$chk/gpfs1/scratch/tmp/osl/e7dd018b54ed4f8dbd2df7b2c7e78b68/CCVPQ_ISQR=     1204.30854949186
+$chkbqia=     21.2447445686288
+$chkbqij=     54.6341481937861
+$chkr0_MP2=    0.293224707618346E-02
+$energy_MP2=    -384.660985381903
+$sccss=    0.333330000000000
+$sccos=     1.20000000000000
+$/gpfs1/scratch/tmp/osl/e7dd018b54ed4f8dbd2df7b2c7e78b68/CC1DENL0R0
+ MP2
+L0 R0 xxx    0    0   -1
+     132.170489342002         0.00000000000000
+     0.00000000000000        0.266197250602352E-01
+    0.486634262276626E-01    0.266197250602352E-01
+$chk/gpfs1/scratch/tmp/osl/e7dd018b54ed4f8dbd2df7b2c7e78b68/CPHF_VPQ_ISQR=     1204.30854949186
+$end
