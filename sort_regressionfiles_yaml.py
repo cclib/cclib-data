746afa5c09f34b7dac55aa8f8dd72ff82810b181
Eric Berquist <eric.berquist@gmail.com>
2024-10-17 10:40:40 -0400

    pre-commit: move logic for applying patch to shell script

--- sort_regressionfiles_yaml.py
+++ sort_regressionfiles_yaml.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+"""sort_regressionfiles_yaml.py: Check and see if the entries in
+regressionfiles.yaml are sorted by filename, producing the diff to standard
+out that should make them sorted.
+
+You should probably call the shell script, which wraps this Python code.
+"""
+
 
 if __name__ == "__main__":
     import sys
@@ -16,6 +23,10 @@ if __name__ == "__main__":
 
     contents_sorted = {"regressions": sorted(contents["regressions"], key=lambda x: x["loc_entry"])}
 
+    # Since the original regressionfiles.yaml has comments, we need to strip
+    # those out before generating the diff, since we can't yet generated
+    # sorted output that contains comments.
+
     with open("regressionfiles_nocomments.yaml", "w", encoding="utf-8") as handle:
         yaml.dump(contents, handle)
 
@@ -24,9 +35,8 @@ if __name__ == "__main__":
 
     result = sp.run(
         [
-            "git",
             "diff",
-            "--no-index",
+            "-u",
             "regressionfiles_nocomments.yaml",
             "regressionfiles_sorted.yaml",
         ]
